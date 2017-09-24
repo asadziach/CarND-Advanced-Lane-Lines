fede906111865eebd52ac1f97c21fe3e7c34c6eb
Asad Zia <asadzia@gmail.com>
2017-09-24 15:45:12 +0500

    Updated Sanity test
    Checking that lanes have similar curvature
    Checking that lanes are separated by approximately the right distance
    horizontally
    Checking that lanes are roughly parallel

--- VideoGenerator.py
+++ VideoGenerator.py
@@ -15,8 +15,10 @@ class VideoLaneProcessor(object):
     window_size = (25,80)     # Obtained empirically
     dpm = (3.7/700, 30/720)   # meters per pixel
     
-    min_lane_distance = 507   # pixles (U.S. regulations)
+    min_lane_distance = 500   # pixles (U.S. regulations)
     max_lane_distane = 659   # pixels
+    
+    curve_tolerance  = 200
     
     def __init__(self, camera_cal_pickle):
         '''
@@ -27,27 +29,7 @@ class VideoLaneProcessor(object):
         self.dist = dest_pickle["dist"]
         
         self.state = LaneState()
-        self.recent_curve_radii = []
-        # was the line detected in the last iteration?
-        self.detected = False  
-        # x values of the last n fits of the line
-        self.recent_xfitted = [] 
-        #average x values of the fitted line over the last n iterations
-        self.bestx = None     
-        #polynomial coefficients averaged over the last n iterations
-        self.best_fit = None  
-        #polynomial coefficients for the most recent fit
-        self.current_fit = [np.array([False])]  
-        #radius of curvature of the line in some units
-        self.radius_of_curvature = None 
-        #distance in meters of vehicle center from the line
-        self.line_base_pos = None 
-        #difference in fit coefficients between last and new fits
-        self.diffs = np.array([0,0,0], dtype='float') 
-        #x values for detected line pixels
-        self.allx = None  
-        #y values for detected line pixels
-        self.ally = None        
+        self.recent_curve_radii = []    
             
     def sanity_ok(self, window_centroids, curve_radii):
         left_center = window_centroids[:,0][0]
@@ -55,6 +37,11 @@ class VideoLaneProcessor(object):
     
         lane_distance = (right_center - left_center)
         if lane_distance < VideoLaneProcessor.min_lane_distance or lane_distance > VideoLaneProcessor.max_lane_distane:
+            return  False
+        
+        left_roc, right_roc = curve_radii
+        diffence = abs(left_roc - right_roc)
+        if diffence > VideoLaneProcessor.curve_tolerance:
             return  False
         return True
     
@@ -76,9 +63,13 @@ class VideoLaneProcessor(object):
         
         if not self.sanity_ok(window_centroids, curve_radii):
             lanes = self.state.recent_lanes[-1]
+            curve_radii = self.recent_curve_radii[-1]
         else:
             self.state.recent_lanes.append(lanes)
-            lanes = np.average(self.state.recent_lanes[-40:], axis=0)
+            self.recent_curve_radii.append(curve_radii)
+            
+        lanes = np.average(self.state.recent_lanes[-40:], axis=0)
+        curve_radii = np.average(self.recent_curve_radii[-5:], axis=0)
 
         result, _ = draw_lane_lines(image, m_inv, lanes, colors=([0,255,0],[0,255,0]))
         
